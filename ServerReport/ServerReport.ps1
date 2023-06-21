d93a5741d31b9fb7ffefdeba190d1448810f055c
AlonzoeFuller <github@7ater.com>
2023-06-21 16:04:00 -0400

    Added commands

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,5 +1,8 @@
 # Retrieves System Name
-$Name = Env:COMPUTERNAME
+$Name = $env:COMPUTERNAME
 
 # OS version info
-($Version = get-computerInfo | Select-Object OsVersion).osversion
+$Version = (get-computerInfo | Select-Object OsVersion).osversion
+
+# Logical Disk Count
+$DiskCount = (Get-CimInstance CIM_LogicalDisk).Count
