edf4b06df2b792fccbf01cb6360cfe47433f13de
AlonzoeFuller <github@7ater.com>
2023-07-05 08:17:17 -0400

    Homework

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -3,13 +3,17 @@ $Name = $env:COMPUTERNAME
 $Version = (get-computerInfo | Select-Object OsVersion).osversion
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).Count
 $FreeSpace = (Get-CimInstance CIM_LogicalDisk).FreeSpace
+$MAC = (Get-NetAdapter).MacAddress
+$Mem = (Get-WMIObject Win32_OperatingSystem).TotalVisibleMemorySize
 
 #Make Our own custom object
 $obj = [PSCustomObject]@{
     ComputerName = $Name
-    DiskCount = $DiskCount
     OSVersion = $Version
-    "GB Free" = $FreeSpace / 1Gb
+    DiskCount = $DiskCount
+    "GB Free" = $FreeSpace 
+    MAC = $MAC
+    Memory = $Mem
 }
 
 #Show Output
