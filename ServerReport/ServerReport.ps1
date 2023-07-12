7c28b51ffd5e9fe197836cee0c2be9d9b5d84b81
AlonzoeFuller <github@7ater.com>
2023-07-12 16:16:34 -0400

    Lots added

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,8 +1,9 @@
 # Retrieves System Info
 $Name = $env:COMPUTERNAME
+$DiskInstance = Get-CimInstance CIM_LogicalDisk
 $Version = (get-computerInfo | Select-Object OsVersion).osversion
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).Count
-$FreeSpace = (Get-CimInstance CIM_LogicalDisk).FreeSpace
+$FreeSpace = ($DiskInstance| Where-Object {$_.DeviceID -eq 'C:'}).Freespace
 $MAC = (Get-NetAdapter).MacAddress
 $Mem = (Get-WMIObject Win32_OperatingSystem).TotalVisibleMemorySize
 $IP4 = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
@@ -12,7 +13,7 @@ $obj = [PSCustomObject]@{
     ComputerName = $Name
     OSVersion = $Version
     DiskCount = $DiskCount
-    "Disk Space Free" = $FreeSpace 
+    "Disk Space Free" = [System.Math]::Round($FreeSpace / 1Gb, 2)
     MAC = $MAC
     Memory = $Mem
     "Ip Address (V4)" = $IP4
