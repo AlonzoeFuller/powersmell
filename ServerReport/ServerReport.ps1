e32b2323ffa37981266ca91c289b5d9917a643e9
AlonzoeFuller <github@7ater.com>
2023-07-12 11:44:14 -0400

    Added more code

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -5,15 +5,17 @@ $DiskCount = (Get-CimInstance CIM_LogicalDisk).Count
 $FreeSpace = (Get-CimInstance CIM_LogicalDisk).FreeSpace
 $MAC = (Get-NetAdapter).MacAddress
 $Mem = (Get-WMIObject Win32_OperatingSystem).TotalVisibleMemorySize
+$IP4 = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
 
 #Make Our own custom object
 $obj = [PSCustomObject]@{
     ComputerName = $Name
     OSVersion = $Version
     DiskCount = $DiskCount
-    "GB Free" = $FreeSpace 
+    "Disk Space Free" = $FreeSpace 
     MAC = $MAC
     Memory = $Mem
+    "Ip Address (V4)" = $IP4
 }
 
 #Show Output
